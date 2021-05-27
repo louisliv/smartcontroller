9187fab393d5df89b1f85b40d3d3a5c8d0afe340
Louis Livingston <livingston.louis@gmail.com>
2021-05-26 20:44:28 -0400

    fixed static

--- server/server/settings.py
+++ server/server/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/server/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR + STATIC_URL
 
 MEDIA_URL = "/media/"
