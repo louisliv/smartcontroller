ac688691da3bb0237fe773f15c562ef27befe397
Louis Livingston <livingston.louis@gmail.com>
2021-05-26 22:42:21 -0400

    build based on electron or no

--- electron-ui/src/environments/environment.prod.ts
+++ electron-ui/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+import { ElectronService } from '../app/core/services';
+
+let isElectron = !!(window && window.process && window.process.type)
+const baseUrlName = isElectron ? "localhost" : window.location.hostname
 export const AppConfig = {
   production: true,
   environment: 'PROD',
-  apiUrl: `http://localhost:8000/api`
+  apiUrl: `http://${baseUrlName}/api`
 };
